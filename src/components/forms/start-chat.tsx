6520c3db17e0b38b4107482b042992bd1cb656ea
Nikkakko <nikolozkopadze@gmail.com>
2024-10-14 23:08:11 +0400

    feat: Add form validation schema
    Add form validation schema for the "message" field in the formSchema object in the validation.ts file.

---
+++ src/components/forms/start-chat.tsx
@@ -0,0 +1,62 @@
+"use client";
+import * as React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { formSchema } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+
+interface StartChatProps {}
+
+const StartChat: React.FC<StartChatProps> = ({}) => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-secondary">
+                Enter your message
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" variant="secondary">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default StartChat;
